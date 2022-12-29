c04322b39d75ba2a87d1a5202c1e7dfa6c046cc8
root <ramanchanna2410@gmail.com>
2022-12-29 16:10:57 +0000

    commit message

---
+++ src/components/VaccinationCoverage/index.js
@@ -0,0 +1,73 @@
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
+
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const dataFormatter = number => {
+    if (number > 1000) {
+      console.log(number)
+
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+  const {vaccinationCoverageDetails} = props
+
+  return (
+    <div className="vaccination-by-coverage-container">
+      <h1 className="vaccination-by-coverage-heading">Vaccination Coverage</h1>
+      <BarChart
+        width={900}
+        height={400}
+        data={vaccinationCoverageDetails}
+        margin={{
+          top: 10,
+        }}
+      >
+        <Bar
+          dataKey="dose1"
+          name="Dose 1"
+          fill="#5a8dee"
+          radius={[10, 10, 0, 0]}
+          barSize="1%"
+        />
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 0.5,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <YAxis
+          tickFormatter={dataFormatter}
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 0.5,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+
+        <Bar
+          dataKey="dose2"
+          name="Dose 2"
+          fill="#f54394"
+          radius={[10, 10, 0, 0]}
+          barSize="20%"
+        />
+        <Legend
+          wrapperStyle={{
+            paddingTop: 100,
+            textAlign: 'center',
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+      </BarChart>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
